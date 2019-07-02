36a98d0246089bfd4256078f020c2fb13faeee95
bob <bob@email.ikv-tech.com>
2019-07-02 10:51:07 +0800

    [CW-HWI] delete

--- CoolwalletProjet/CoolwalletLib/CwTransport.py
+++
@@ -1,9 +0,0 @@
-# This is an abstract class that defines all of method that each CoolWallet 
-# Transport must implement
-class CoolWalletTransport:
-
-    def CwWrite(self, cmd, data):
-        raise NotImplementedError('The CoolWalletTransport base class does not implement this method')
-
-    def CwRead(self):
-        raise NotImplementedError('The CoolWalletTransport base class does not implement this method')
